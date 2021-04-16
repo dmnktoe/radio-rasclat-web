0210cc9635fb62145838922bde2442b18e519772
Domenik Töfflinger <domenikt96@yahoo.de>
2021-04-16 17:59:07 +0200

    create Projects.ts Model

---
+++ src/app/core/models/Project.ts
@@ -0,0 +1,11 @@
+export class Project {
+  _id: string;
+  title: string;
+  description: string;
+  timeStart: Date;
+  timeEnd: Date;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+  slug: string;
+}
