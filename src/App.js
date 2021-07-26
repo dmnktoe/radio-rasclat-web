71606bcb0ccfab8812f53b1b16c8c873601b3756
Domenik Töfflinger <domenikt96@yahoo.de>
2021-07-26 08:09:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,19 @@ import Gradient from './components/Gradient';
 import styled from 'styled-components';
 import Marquee3k from 'marquee3000';
 
-const Main = styled.div`
-  position: relative;
-  overflow-x: hidden;
-  color: #ffffff;
-`
-
 function App() {
   Marquee3k.init()
   return (
     <div className="App">
-      <Gradient/>
-      <Main>
-        <Header/>
-        <Container>
-          <div className="marquee3k" data-speed="1.25">
-            <h3>Radio Rasclat&nbsp;→&nbsp;alles auf einen Blick&nbsp;→&nbsp;</h3>
-          </div>
-          <Box>
-            Cookies!
-          </Box>
-        </Container>
-      </Main>
+      <Header/>
+      <Container>
+        <div className="marquee3k" data-speed="0.25">
+          <h3>Radio Rasclat&nbsp;→&nbsp;alles auf einen Blick&nbsp;→&nbsp;</h3>
+        </div>
+        <Box>
+          Cookies!
+        </Box>
+      </Container>
     </div>
   );
 }
